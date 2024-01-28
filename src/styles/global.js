0a5acace048a38c17d8083c8beda3ed7a5534d77
welesonbatista <weleson.dev@gmail.com>
2024-01-28 19:27:53 -0300

    first commit

---
+++ src/styles/global.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    body{
+        background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800} ;
+    }
+    a{
+        text-decoration: none;
+
+    }
+    button, a {
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+    button:hover, a:hover {
+        transition: brightness(0.9);
+    }
+
+    `
