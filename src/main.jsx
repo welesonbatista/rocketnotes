1dc8e5c68da6691fe2b024903b1f01ed2d5d5794
welesonbatista <weleson.dev@gmail.com>
2024-01-30 15:55:46 -0300

    finish project

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
-import { Details } from './pages/Details/'
 import GlobalStyles from './styles/global'
+
 import theme from './styles/theme'
 
+import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,24 +1,35 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-    *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-    body{
-        background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800} ;
-    }
-    a{
-        text-decoration: none;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-    }
-    button, a {
-        cursor: pointer;
-        transition: filter 0.2s;
-    }
-    button:hover, a:hover {
-        transition: brightness(0.9);
-    }
+  body {
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    color: ${({ theme }) => theme.COLORS.WHITE};
 
-    `
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button, textarea {
+    font-family: 'Roboto Slab', serif;
+    font-size: 16px;
+    outline: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter 0.2s;
+  }
+
+  button:hover, a:hover {
+    filter: brightness(0.9);
+  }
+`
