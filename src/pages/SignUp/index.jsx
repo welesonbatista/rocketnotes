2f272489b508762aa9de6f1912d5229370737595
welesonbatista <weleson.dev@gmail.com>
2024-03-19 18:27:50 -0300

    Add sign up form and functionality

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -8,7 +8,14 @@ import { Button } from '../../components/Button'
 import { Container, Form, Background } from './styles'
 
 export function SignUp() {
-  const [] = useState();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignUp() {
+    console.log(name, email, password)
+  }
+
   return (
     <Container>
       <Background />
@@ -23,21 +30,24 @@ export function SignUp() {
           placeholder="Nome"
           type="text"
           icon={FiUser}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Cadastrar" />
+        <Button title="Cadastrar" onClick={handleSignUp} />
 
         <Link to="/">Voltar para o login</Link>
       </Form>
