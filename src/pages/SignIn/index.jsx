8336f1619898a4a9674e4df2387706193977b55f
welesonbatista <weleson.dev@gmail.com>
2024-03-28 14:39:18 -0300

    setState

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { FiMail, FiLock } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 
@@ -9,9 +10,14 @@ import { Button } from '../../components/Button'
 import { Container, Form, Background } from './styles'
 
 export function SignIn() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const { signIn } = useAuth();
 
-  const data = useAuth();
-  console.log("Meu contexto", data)
+  function handleSignIn() {
+    SignIn({ email, password })
+  }
+
   return (
     <Container>
       <Form>
@@ -24,15 +30,17 @@ export function SignIn() {
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">Criar conta</Link>
       </Form>
