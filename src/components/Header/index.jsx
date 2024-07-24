6a109820d656d6e93fd7e954026fe0e0a50d552f
Weleson Batista <weleson.dev@gmail.com>
2024-07-24 19:06:23 -0300

    aaa

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,19 @@
 import { RiShutDownLine } from 'react-icons/ri'
 import { Container, Profile, Logout } from './styles'
 import { useAuth } from '../../hooks/auth'
+import { api } from '../../services/api';
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
 
 export function Header() {
-  const {signOut} = useAuth();
+  const {signOut, user} = useAuth();
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
   return (
     <Container>
       <Profile to="/profile">
         <img
-          src="https://github.com/welesonbatista.png"
+          src={avatarUrl}
           alt="Foto do usuário"
         />
 
